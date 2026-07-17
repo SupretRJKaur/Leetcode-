2619e76360719b5a821732e47a4bfcc26ab4b050
Supreet <148531788+SupretRJKaur@users.noreply.github.com>
2026-07-17 23:21:22 +0530

    Time: 111 ms (98.86%) | Memory: 17 MB (50.34%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def calculate(self, s):
+        s = s.replace(" ", "") + "+"
+        stack = []
+        j = 0    
+        i = "+"  
+        
+        for char in s:
+            if char.isdigit():
+                j = (j * 10) + int(char)
+            else:
+                if i == "+":
+                    stack.append(j)
+                elif i == "-":
+                    stack.append(-j)
+                elif i == "*":
+                    stack.append(stack.pop() * j)
+                elif i == "/":
+                    prev = stack.pop()
+                    if prev < 0:
+                        stack.append(-(-prev // j))
+                    else:
+                        stack.append(prev // j)
+                
+                i = char
+                j = 0
+                
+        return sum(stack)
