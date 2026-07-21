110c535ea6c950a80246f6bfcb9bbc994c837f4f
Supreet <148531788+SupretRJKaur@users.noreply.github.com>
2026-07-21 23:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (66.32%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,10 +1,7 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        # Loop through every row
         for row in matrix:
-            # Loop through every element in the current row
             for element in row:
                 if element == target:
                     return True
-                    
         return False
