9fa266470cdd9fc2704d7360437e36912cb19da7
Supreet <148531788+SupretRJKaur@users.noreply.github.com>
2026-07-31 13:15:10 +0530

    Time: 52 ms (5.94%) | Memory: 19.9 MB (10.51%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        visited= set()
+        curr=head
+        while curr:
+            if curr in visited:
+                return True
+            visited.add(curr)
+            curr= curr.next
+        return False
