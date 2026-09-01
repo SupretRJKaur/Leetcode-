025e4718204604086ebbfce6c0a375ed997704c2
Supreet <148531788+SupretRJKaur@users.noreply.github.com>
2026-09-01 22:29:04 +0530

    Time: 11 ms (56.24%) | Memory: 14.2 MB (72.38%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,16 @@
+from collections import Counter
+import heapq
+
+
+class Solution:
+
+    def topKFrequent(self, nums, k):
+        count = Counter(nums)
+        min_heap = []
+
+        for num, freq in count.items():
+            heapq.heappush(min_heap, (freq, num))
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        return [num for freq, num in min_heap]
