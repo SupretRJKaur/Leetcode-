799d7b678931c9c7284e2dd56751b74dd08080d4
Supreet <148531788+SupretRJKaur@users.noreply.github.com>
2026-09-01 22:29:43 +0530

    Time: 19 ms (17.88%) | Memory: 20.1 MB (8.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,23 @@
 from collections import Counter
-import heapq
 
 
 class Solution:
 
     def topKFrequent(self, nums, k):
+        # 1. Count frequencies
         count = Counter(nums)
-        min_heap = []
+
+        # 2. Bucket where index = frequency
+        # max possible frequency is len(nums)
+        buckets = [[] for _ in range(len(nums) + 1)]
 
         for num, freq in count.items():
-            heapq.heappush(min_heap, (freq, num))
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
+            buckets[freq].append(num)
 
-        return [num for freq, num in min_heap]
+        # 3. Gather top k elements from highest frequency bucket down to lowest
+        res = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
