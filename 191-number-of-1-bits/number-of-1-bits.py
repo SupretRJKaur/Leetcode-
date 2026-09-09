c1c09fd28847d65c774abc07b536a4c60335b901
Supreet <148531788+SupretRJKaur@users.noreply.github.com>
2026-09-09 21:18:50 +0530

    Time: 2 ms (24.02%) | Memory: 12.3 MB (55.65%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -3,6 +3,6 @@ class Solution:
     def hammingWeight(self,n):
         count = 0
         while n > 0:
-            n = n & (n - 1)  # Drops the lowest set bit (1)
+            n = n & (n - 1)  
             count += 1
         return count
