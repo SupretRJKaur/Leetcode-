63188d84d129aad84d70751ed2307566a947ba29
Supreet <148531788+SupretRJKaur@users.noreply.github.com>
2026-09-20 23:06:27 +0530

    Time: 46 ms (56.63%) | Memory: 13.8 MB (56.28%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,40 +1,33 @@
 class Solution:
 
     def pacificAtlantic(self, heights):
-        if not heights or not heights[0]:
-            return []
-
-        rows = len(heights)
-        cols = len(heights[0])
-
+        rows, cols = len(heights), len(heights[0])
         pac = set()
         atl = set()
 
-        def backtrack(r, c, visit, prev_height):
-            if r < 0 or r >= rows or c < 0 or c >= cols:
-                return
-
-            if (r, c) in visit or heights[r][c] < prev_height:
-                return
-
+        def dfs(r, c, visit):
             visit.add((r, c))
 
-            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            for dr, dc in directions:
-                backtrack(r + dr, c + dc, visit, heights[r][c])
+            if r + 1 < rows and (r + 1, c) not in visit and heights[r + 1][c] >= heights[r][c]:
+                dfs(r + 1, c, visit)
 
+            if r - 1 >= 0 and (r - 1, c) not in visit and heights[r - 1][c] >= heights[r][c]:
+                dfs(r - 1, c, visit)
+
+            if c + 1 < cols and (r, c + 1) not in visit and heights[r][c + 1] >= heights[r][c]:
+                dfs(r, c + 1, visit)
+
+            if c - 1 >= 0 and (r, c - 1) not in visit and heights[r][c - 1] >= heights[r][c]:
+                dfs(r, c - 1, visit)
+
+        for r in range(rows):
+            dfs(r, 0, pac)
         for c in range(cols):
-            backtrack(0, c, pac, heights[0][c])
-            backtrack(rows - 1, c, atl, heights[rows - 1][c])
+            dfs(0, c, pac)
 
         for r in range(rows):
-            backtrack(r, 0, pac, heights[r][0])
-            backtrack(r, cols - 1, atl, heights[r][cols - 1])
+            dfs(r, cols - 1, atl)
+        for c in range(cols):
+            dfs(rows - 1, c, atl)
 
-        result = []
-        for r in range(rows):
-            for c in range(cols):
-                if (r, c) in pac and (r, c) in atl:
-                    result.append([r, c])
-
-        return result
+        return [[r, c] for r in range(rows) for c in range(cols) if (r, c) in pac and (r, c) in atl]
